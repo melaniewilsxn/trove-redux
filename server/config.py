58ea2fe30d81cdf05f40cc0620ba8f96ea4f3b6c
Melanie Wilson <melaniemwilson1@gmail.com>
2024-05-30 20:46:10 -0400

    deployment changes

--- server/config.py
+++ server/config.py
@@ -3,7 +3,7 @@ from os import environ, path
 from dotenv import load_dotenv
 
 # Remote library imports
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
@@ -11,12 +11,13 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# Local imports
-basedir = path.abspath(path.dirname(__file__))
+# Set the base directory to two levels up (root directory)
+basedir = path.abspath(path.join(path.dirname(__file__), '..'))
+
+# Load environment variables from the .env file located in the root directory
 load_dotenv(path.join(basedir, '.env'))
 
-# Instantiate app, set attributes
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../client/build", static_url_path="/")
 app.secret_key = environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') == 'True'
@@ -37,3 +38,12 @@ api = Api(app)
 
 # Instantiate CORS
 CORS(app)
+
+# Serve React App
+@app.route('/')
+@app.route('/<path:path>')
+def serve_react_app(path=""):
+    if path and path != "favicon.ico":
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
